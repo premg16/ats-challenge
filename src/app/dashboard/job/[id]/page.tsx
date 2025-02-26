d65b8f194804d9554509127abbbac1593d7ed0b9
premg16 <premsaig1605@gmail.com>
2025-02-27 03:27:40 +0530

    removed unnecessary files

--- src/app/dashboard/job/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getJob } from "@/lib/prisma/jobs/getJobs";
-import JobView from "@/components/views/job-view";
-
-export default async function Page({ params }: { params: { id: string } }) {
-  const job = await getJob(parseInt(params.id));
-  return job && <JobView job={job} />;
-}
--- src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getJobs } from "@/lib/prisma/jobs/getJobs";
-import DashboardView from "@/components/views/dashboard-view";
-
-export default async function Page() {
-  const jobs = await getJobs();
-  return <DashboardView jobs={jobs} />;
-}
